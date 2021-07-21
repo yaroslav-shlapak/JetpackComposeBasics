a64e378cb14074871be4925460b62b32898b0a4f
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-07-21 16:33:28 +0300

    Lesson 1

---
+++ app/src/main/java/com/test/jetpackcompose/lesson1/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.test.jetpackcompose.lesson1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.test.jetpackcompose.lesson1.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text (text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting() {
+    Greeting("Yaroslav")
+}
