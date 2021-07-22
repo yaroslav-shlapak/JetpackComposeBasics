bb082458e69fb96b85c255debb1df5945ae6741e
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-07-22 20:31:58 +0300

    Lesson 3

--- app/src/main/java/com/test/jetpackcompose/lesson1/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/lesson1/MainActivity.kt
@@ -9,49 +9,63 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.test.jetpackcompose.lesson1.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    NewsStory()
-                }
-            }
+            NewsStory()
         }
+
     }
 }
 
 @Composable
 fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.header),
-            contentDescription = null,
-            modifier = Modifier
-                .height(180.dp)
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.header),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+            Text(
+                text = "A day wandering through the sandhills " +
+                        "in Shark Fin Cove, and a few of the " +
+                        "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "Davenport, California",
+                style = typography.body2
+            )
+            Text(
+                text = "December 2018",
+                style = typography.body2
+            )
+        }
     }
 }
 
