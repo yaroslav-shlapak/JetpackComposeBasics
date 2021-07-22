3577f7c0ce633df756dfc8c4505910ac3bca8a1a
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-07-22 20:08:16 +0300

    Lesson 2

--- app/src/main/java/com/test/jetpackcompose/lesson1/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/lesson1/MainActivity.kt
@@ -3,11 +3,21 @@ package com.test.jetpackcompose.lesson1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.test.jetpackcompose.lesson1.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,21 +27,36 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    NewsStory()
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String) {
-    Text (text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
 }
 
 @Preview
 @Composable
 fun PreviewGreeting() {
-    Greeting("Yaroslav")
+    NewsStory()
 }
